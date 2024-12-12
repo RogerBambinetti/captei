6e18bea049190155a0bb4cd9912a19b13c90124e
Roger <rogerbambinetti@hotmail.com>
2024-12-11 23:48:25 -0300

    populate portal table

---
+++ src/db/migrations/20241212024407_populate-portals.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    return knex("portal").insert({ name: 'Duda Imóveis', url: 'teste', observations: 'teste' });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    return knex("portal").delete()
+};
