4ceaaca2dd4fe90f83d0e402dcca4874c8c144e4
Roger <rogerbambinetti@hotmail.com>
2024-12-12 21:38:04 -0300

    use https

--- src/es/elasticsearch.js
+++ src/es/elasticsearch.js
@@ -2,6 +2,10 @@ const { Client } = require('@elastic/elasticsearch')
 
 const client = new Client({
     node: process.env.ES_HOST,
+    auth: {
+        username: process.env.ES_USER,
+        password: process.env.ES_PASSWORD
+    },
     tls: {
         rejectUnauthorized: false,
     }
